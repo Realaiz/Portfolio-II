57d76354ede5edc904309b244ab3bd8dd298c48e
Leo Sharif <Realaiz@users.noreply.github.com>
2024-06-23 18:42:10 +1000

    added parallax, still on hompeage seems like im experiencing some scope creep

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,22 @@
 import Image from 'next/image';
 import heroImage from '../public/hero-desktop.jpg'
-import lionBanner from '../public/lionpride-nobg.png'
-import LogoAnimation from './ui/logo';
+import lionBanner from '../public/lion-foreground.png'
 import Title from './ui/title';
 
-
+        {/*<div className="relative h-32 bg-darknavy blur-2xl z-10"/>*/}
 export default function Home() {
   return (
-    <main>
-      {/*Top hero section - mainly just cosmetic details and a splash */}
-      <section>
-        <div className="relative h-32 bg-darknavy blur-2xl z-10"></div>
-        <div className="relative -mt-32 flex flex-col items-center">
-          <Image src={heroImage} sizes="100vw" alt="Hero image of blacksmith at a forge with forest scenery" className="w-full h-auto"/>
-          <div className="absolute flex flex-col items-center mt-48" >
-            <LogoAnimation />
-            <Title />
-          </div>
-        </div>
+    <main className="h-screen overflow-y-auto overflow-x-hidden perspective"> 
+      {/*Top hero section - mainly just cosmetic details and a splash  divwrapper above and header below*/}
+      <section className="relative h-full display-flex justify-center items-center preserve-3d -z-10">
+        <Image src={heroImage} alt="Hero image of blacksmith at a forge with forest scenery" className="absolute h-full w-full object-cover -z-1 background-parallax"/>
+        <Image src={lionBanner} alt="static image of a pride of lions" className="absolute h-full w-full object-cover -z-1 foreground-parallax mt-44" />
+        <Title />
       </section>
 
-      <div className="relative -mt-72 z-10">
-        <Image src={lionBanner} sizes="100vw" alt="static image of a pride of lions" className="w-full h-auto" />
-      </div>
       {/* Section section - intro & interests & mayb some projs */}
-      <section className="flex flex-col items-center h-64 bg-darknavy text-white py-8">
-        <div className="flex flex-col items-center text-center">
-          <h2 className="text-2xl font-bold mb-2">Hi I'm Leo, enjoyer of Math, Finance & CS with a Bsc Actuarial Science from Bond Uni.</h2>
-          <p className="text-lg mb-4">I dabble in cooking, guitar, acting & tennis.</p>
-          <div className="flex justify-center space-x-4 mb-4">
-          </div>
-        </div>
+      <section className="text">
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente itaque deserunt inventore eos molestiae, omnis quas beatae sint reprehenderit nihil, distinctio corrupti pariatur sequi. Saepe libero cumque quis repellat possimus, similique sit a voluptatem debitis? Quasi quod facilis excepturi voluptatem iusto alias autem quo commodi eius explicabo, quos hic ut sapiente sit harum, inventore pariatur dignissimos minus facere, aliquam incidunt itaque veritatis. Sed sunt excepturi sapiente quasi iure eos esse animi ipsa itaque ipsum eligendi dolores distinctio magni architecto nam porro ea culpa, ab nostrum placeat facere, cupiditate similique illum? Quia itaque reprehenderit laudantium et maiores repudiandae ab ex, voluptatum non inventore iste illum minima sint nostrum cum unde sed quo distinctio eius optio? Corporis praesentium ipsa repellendus reiciendis totam, tempora eligendi illo eos, corrupti similique deserunt perspiciatis nesciunt excepturi provident. Fugit molestiae facilis at nesciunt voluptates totam, in vero, veniam, minus debitis modi consequuntur neque hic vel aperiam doloribus esse amet nostrum commodi cupiditate labore. Quia omnis a voluptas voluptate deleniti labore mollitia veniam eaque. Cupiditate labore placeat similique quam dolor sed, quis deserunt quidem sapiente adipisci corrupti, optio porro commodi at atque non omnis obcaecati esse perspiciatis nostrum nobis facere sunt nam quos. Ad ipsam quia architecto tenetur, incidunt minus maxime exercitationem, quisquam fugit soluta facere eaque atque perferendis temporibus dolorum totam enim accusantium neque veniam dignissimos magni explicabo commodi! Fugit accusantium non mollitia placeat quam eaque rerum porro cumque sequi eos explicabo esse libero maxime alias doloribus exercitationem quas ab, quod animi fuga. Cum amet mollitia tempore. Doloremque saepe possimus veritatis voluptates, ab autem labore similique libero expedita porro quis rerum repellendus, quod quam quaerat tempore pariatur sequi unde. Debitis atque quisquam modi. Optio, aspernatur quidem, sit possimus pariatur consequatur nulla facilis laudantium, tempora natus asperiores non voluptate in tenetur temporibus nisi beatae odio! Illo officia eum earum? Itaque iusto voluptatibus sit ex incidunt, ea ratione, fugit id aliquam adipisci repellendus facilis laudantium. Sint, expedita ipsa! Sequi, provident blanditiis repudiandae laudantium dolorem ipsa odio numquam pariatur esse labore facilis doloribus sit aliquam earum. Iste quas beatae repellat dolore quae ullam porro laudantium quo nam aliquid consequuntur ea amet harum, cupiditate debitis nisi, eaque, id aut suscipit! Nisi!</h1>
       </section>
 
     </main>
--- app/ui/title.tsx
+++ app/ui/title.tsx
@@ -1,12 +1,16 @@
 'use client';
 import useScrollHandler from "./hooks/useScrollHandler"
+import LogoAnimation from "./logo";
 
 export default function Title() {
   const scrollPosition = useScrollHandler();
   const translateY = Math.min(scrollPosition, 200); // limit the translation
   const opacity = Math.max(1 - scrollPosition / 200, 0); // fade out effect
   return (
-    <h1 className="font-bold font-mono text-7xl text-lightblue hero-title mt-4"
-    style={{ transform: `translateY(${translateY}px)`, opacity: opacity,}}>Leo Sharif</h1>
+    <div className="absolute flex flex-col items-center top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/3" >
+      <LogoAnimation />  
+      <h1 className="font-bold font-mono text-7xl text-lightblue hero-title mt-4"
+      style={{ transform: `translateY(${translateY}px)`, opacity: opacity,}}>Leo Sharif</h1>
+    </div>
   );  
 };
